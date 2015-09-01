cb45ae00cb572cc02fb249f2a79876acf54471cf
Gerardo Teruel <gerardogtn@gmail.com>
2015-09-01 17:16:14 -0500

    File to verify that each algorithm works properly

---
+++ Homework/02MeasuringAlgorithms/testSort.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include "sortingAlgorithms.h"
+void printArray(int array[], int n);
+
+int main(int argc, const char * argv[])
+{
+  int n = getArraySize();
+  int * arrayToSort = new int[n];
+  addNRandomElements(arrayToSort, n);
+  printArray(arrayToSort, n);
+  countingSort(arrayToSort, n);
+  std::cout  << std::endl;
+  printArray(arrayToSort, n);
+  delete arrayToSort;
+  return 0;
+}
+
+void printArray(int array[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    if (i % 10 == 9)
+    {
+      std::cout << array[i] << "  " << std::endl;
+    }
+    else
+    {
+      std::cout << array[i] << "  ";
+    }
+  }
+}
