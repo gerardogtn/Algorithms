17f038a896de23a3a210c2ad313390f95c2d1ee2
Gerardo Teruel <gerardogtn@gmail.com>
2015-09-01 17:15:55 -0500

    Main file to hand in

---
+++ Homework/02MeasuringAlgorithms/homework2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "sortingAlgorithms.h"
+
+int main(int argc, const char * argv[])
+{
+  int n = getArraySize();
+  int arrayToSort[n];
+  int algorithm = getAlgorithmNumber();
+
+  addNRandomElements(arrayToSort, n);
+
+  void (*sortingAlgorithm)(int *, int) = nullptr;
+
+  switch (algorithm) {
+    case 1:
+      sortingAlgorithm = bubbleSort;
+      break;
+    case 2:
+      break;
+    case 3:
+      sortingAlgorithm = insertionSort;
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      sortingAlgorithm = mergeSort;
+      break;
+    case 7:
+      break;
+    case 8:
+      break;
+    case 9:
+      break;
+    case 10:
+      sortingAlgorithm = selectionSort;
+      break;
+    case 11:
+      break;
+    case 12:
+      sortingAlgorithm = quickSort;
+    default:
+      break;
+  }
+
+  if (sortingAlgorithm == nullptr)
+  {
+    std::cout << "Algorithm number not valid" << std::endl;
+  }
+  else {
+    printExecutionTime(arrayToSort, n, sortingAlgorithm);
+  }
+
+  return 0;
+}
