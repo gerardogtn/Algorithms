fdac92665468ca9633755f5bdb90f590c064f03f
Gerardo Teruel <gerardogtn@gmail.com>
2015-09-05 15:55:48 -0500

    :rocket: Update tests for height, level, depth, balancefactor

--- DataStructures/Trees/testBst.cpp
+++ DataStructures/Trees/testBst.cpp
@@ -19,6 +19,7 @@ int main(int argc, const char * argv[])
   bst->insert(six);
   bst->insert(nine);
   bst->insert(ten);
+  bst->insert(9);
 
   std::cout << "Imprimiendo en orden ascendente" << std::endl;
   bst->inOrder();
@@ -30,11 +31,27 @@ int main(int argc, const char * argv[])
   std::cout << (bst->search(6)? "Encontre 6" : "No encontre 6") << std::endl;
   std::cout << (bst->search(8)? "Encontre 8" : "No encontre 8") << std::endl;
 
-  std::cout << "Altura del arbol: " << bst->getHeight() << std::endl;
+  //std::cout << "Insertando 8" << std::endl;
+  //bst->insert(11);
+
+  std::cout << (bst->search(8)? "Encontre 8" : "No encontre 8") << std::endl;
+
+  std::cout << "============= TEST HEIGHT, DEPTH, LEVEL ==================" << std::endl;
+  std::cout << "Altura de la raiz: " << bst->getHeight() <<  std::endl;
+  std::cout << "Profundidad de la raiz: " << bst->getDepth() <<  std::endl;
+  std::cout << "Nivel de la raiz: " << bst->getLevel() <<  std::endl;
 
   std::cout << "Altura del elemento 10: " << bst->getHeight(ten) << std::endl;
   std::cout << "Profundidad del elemento 10: " << bst->getDepth(ten) << std::endl;
   std::cout << "Nivel del elemento 10: " << bst->getLevel(ten) << std::endl;
+
+  std::cout << "=============== TEST BALANCE FACTOR ====================" << std::endl;
+  std::cout << "Balance Factor, elemento 5: " << bst->getBalanceFactor(five)<< std::endl;
+  std::cout << "Balance Factor, elemento 3: " << bst->getBalanceFactor(three)<< std::endl;
+  std::cout << "Balance Factor, elemento 4: " << bst->getBalanceFactor(four)<< std::endl;
+  std::cout << "Balance Factor, elemento 6: " << bst->getBalanceFactor(six)<< std::endl;
+  std::cout << "Balance Factor, elemento 9: " << bst->getBalanceFactor(nine)<< std::endl;
+  std::cout << "Balance Factor, elemento 10: " << bst->getBalanceFactor(ten)<< std::endl;
 
   bst->clear();
 
