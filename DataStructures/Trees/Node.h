c1c1d4fd1ce6771dd817e6595b02bfa41fde0fcc
Gerardo Teruel <gerardogtn@gmail.com>
2015-09-19 20:52:22 -0500

    :rocket: Update destructor and constructor

--- DataStructures/Trees/Node.h
+++ DataStructures/Trees/Node.h
@@ -10,8 +10,8 @@ class Node
 private:
   // Number of records in each node (if count != 0, node has count + 1 children)
   int count;
-  Record *data;
-  Node<Record, order>  *children;
+  Record **data;
+  Node<Record, order>  **children;
 
 public:
   Node();
@@ -23,15 +23,35 @@ template <class Record, int order>
 Node<Record, order>::Node()
 {
   this->count = 0;
-  this->data = new Record[order - 1];
-  this->children = new Node[order];
+  this->data = new Record *[order - 1];
+  this->children = new Node *[order];
+
+  for (int i = 0; i < order - 1; i++){
+    data[i] = nullptr;
+    children[i] = nullptr;
+  }
+  children[order - 1] = nullptr;
 }
 
 template <class Record, int order>
 Node<Record, order>::~Node()
 {
-  delete this->data;
-  delete this->children;
+  for (int i = 0; i < order - 1; i++)
+  {
+    if (data[i]){
+      delete data[i];
+    }
+    if (children[i]){
+      delete children[i];
+    }
+  }
+
+  if (children[order - 1]){
+    delete children[order - 1];
+  }
+
+  delete[] data;
+  delete[] children;
 }
 
 #endif
