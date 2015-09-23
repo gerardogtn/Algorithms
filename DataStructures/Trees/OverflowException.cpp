790c657fd099906e20e23e866be37cdbb68fbf39
Gerardo Teruel <gerardogtn@gmail.com>
2015-09-23 09:53:18 -0500

    :see_no_evil: Add public constructor and make what() public

--- DataStructures/Trees/OverflowException.cpp
+++ DataStructures/Trees/OverflowException.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <exception>
+#include <stdexcept>
+#include <sstream>
 using namespace std;
 
 // This exception was created with the BTree.h in mind
-class OverflowException: public exception
+class OverflowException: public runtime_error
 {
+public:
+  OverflowException() : runtime_error("Overflow exception"){}
+
   virtual const char* what() const throw()
   {
     return "There was no room for insertion!";
