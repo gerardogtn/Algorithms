b1b526c0c2aa1abb180d4a32f0e34a76ccc9c9c8
Gerardo Teruel <gerardogtn@gmail.com>
2015-09-22 19:43:20 -0500

    :rocket: Add Overflow Exception

---
+++ DataStructures/Trees/OverflowException.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+// This exception was created with the BTree.h in mind
+class KeyNotFoundException: public exception
+{
+  virtual const char* what() const throw()
+  {
+    return "There was no room for insertion!";
+  }
+};
