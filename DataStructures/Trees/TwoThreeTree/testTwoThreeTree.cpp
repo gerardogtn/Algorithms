8f5eb4194d6e1b365a4d5f14b407063b59e84eda
Gerardo Teruel <gerardogtn@gmail.com>
2015-09-30 17:47:57 -0500

    :rocket: Add test file for TwoThreeTree

---
+++ DataStructures/Trees/TwoThreeTree/testTwoThreeTree.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include "TwoThreeTree.h"
+
+// This function tests the example at Kruse's textbook.
+// Makes a BTree of order 5 representation of the English alphabet.
+int main(int argc, const char * argv[])
+{
+  TwoThreeTree<char> * tree = new TwoThreeTree<char>();
+  std::cout << "====================== DECLARANDO ARBOL \n" << std::endl;
+  tree->printAscending();
+  tree->printDescending();
+
+  std::cout << "====================== INSERTANDO 4 ELEMENTOS" << std::endl;
+  tree->insert('a');
+  tree->insert('g');
+  tree->insert('f');
+  tree->insert('b');
+  std::cout << "A, B, F, G: insertados \n" << std::endl;
+  tree->printAscending();
+  tree->printDescending();
+
+  std::cout << "====================== BUSCANDO ELEMENTOS" << std::endl;
+  std::cout << (tree->search('a')? "Encontre A": "No encontre A") << std::endl;
+  std::cout << (tree->search('b')? "Encontre B": "No encontre B") << std::endl;
+  std::cout << (tree->search('c')? "Encontre C": "No encontre C") << std::endl;
+  std::cout << (tree->search('d')? "Encontre D": "No encontre D") << std::endl;
+  std::cout << (tree->search('e')? "Encontre E": "No encontre E") << std::endl;
+  std::cout << (tree->search('f')? "Encontre F": "No encontre F") << std::endl;
+  std::cout << (tree->search('g')? "Encontre G": "No encontre G") << std::endl;
+  std::cout << (tree->search('h')? "Encontre H": "No encontre H") << std::endl;
+  std::cout << (tree->search('i')? "Encontre I": "No encontre I") << std::endl;
+  std::cout << (tree->search('j')? "Encontre J": "No encontre J") << std::endl;
+  std::cout << (tree->search('k')? "Encontre K": "No encontre K") << std::endl;
+  std::cout << (tree->search('l')? "Encontre L": "No encontre L") << std::endl;
+  std::cout << (tree->search('m')? "Encontre M": "No encontre M") << std::endl;
+
+  std::cout << "====================== INSERTANDO ELEMENTO EXTRA" << std::endl;
+  tree->insert('k');
+  tree->insert('d');
+  tree->insert('h');
+  tree->insert('m');
+  tree->insert('j');
+  std::cout << "K D H M J: insertada \n" << std::endl;
+  tree->printAscending();
+  tree->printDescending();
+
+  std::cout << "====================== BUSCANDO ELEMENTOS" << std::endl;
+  std::cout << (tree->search('a')? "Encontre A": "No encontre A") << std::endl;
+  std::cout << (tree->search('b')? "Encontre B": "No encontre B") << std::endl;
+  std::cout << (tree->search('c')? "Encontre C": "No encontre C") << std::endl;
+  std::cout << (tree->search('d')? "Encontre D": "No encontre D") << std::endl;
+  std::cout << (tree->search('e')? "Encontre E": "No encontre E") << std::endl;
+  std::cout << (tree->search('f')? "Encontre F": "No encontre F") << std::endl;
+  std::cout << (tree->search('g')? "Encontre G": "No encontre G") << std::endl;
+  std::cout << (tree->search('h')? "Encontre H": "No encontre H") << std::endl;
+  std::cout << (tree->search('i')? "Encontre I": "No encontre I") << std::endl;
+  std::cout << (tree->search('j')? "Encontre J": "No encontre J") << std::endl;
+  std::cout << (tree->search('k')? "Encontre K": "No encontre K") << std::endl;
+  std::cout << (tree->search('l')? "Encontre L": "No encontre L") << std::endl;
+  std::cout << (tree->search('m')? "Encontre M": "No encontre M") << std::endl;
+
+  std::cout << "====================== ELIMINANDO ELEMENTO EXTRA" << std::endl;
+  tree->remove('k');
+  tree->remove('d');
+  tree->remove('h');
+  tree->remove('m');
+  tree->remove('j');
+  std::cout << "K D H M J: borradas \n" << std::endl;
+  tree->printAscending();
+  tree->printDescending();
+
+  std::cout << "====================== BUSCANDO ELEMENTOS" << std::endl;
+  std::cout << (tree->search('a')? "Encontre A": "No encontre A") << std::endl;
+  std::cout << (tree->search('b')? "Encontre B": "No encontre B") << std::endl;
+  std::cout << (tree->search('c')? "Encontre C": "No encontre C") << std::endl;
+  std::cout << (tree->search('d')? "Encontre D": "No encontre D") << std::endl;
+  std::cout << (tree->search('e')? "Encontre E": "No encontre E") << std::endl;
+  std::cout << (tree->search('f')? "Encontre F": "No encontre F") << std::endl;
+  std::cout << (tree->search('g')? "Encontre G": "No encontre G") << std::endl;
+  std::cout << (tree->search('h')? "Encontre H": "No encontre H") << std::endl;
+  std::cout << (tree->search('i')? "Encontre I": "No encontre I") << std::endl;
+  std::cout << (tree->search('j')? "Encontre J": "No encontre J") << std::endl;
+  std::cout << (tree->search('k')? "Encontre K": "No encontre K") << std::endl;
+  std::cout << (tree->search('l')? "Encontre L": "No encontre L") << std::endl;
+  std::cout << (tree->search('m')? "Encontre M": "No encontre M") << std::endl;
+
+
+  delete tree;
+
+  return 0;
+}
